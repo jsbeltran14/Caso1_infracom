3c98ae948e1f814a117ea47e1301b62ec30b1927
SEBASTIAN BELTRAN MORALES <js.beltran14@uniandes.edu.co>
2019-02-28 20:23:24 -0500

    servidor

--- Caso1/src/Caso1/Main/Servidor.java
+++ Caso1/src/Caso1/Main/Servidor.java
@@ -1,5 +1,7 @@
 package Caso1.Main;
 
+
+
 public class Servidor extends Thread{
 	private int id;
 	private Buffer buffer;
@@ -10,17 +12,35 @@ public class Servidor extends Thread{
 		this.buffer=buffer;
 	}
 	
-	public void run()
-	{
-		while(true)
+	public void run(){
+		synchronized (this)
 		{
-			if(buffer.getCantidadClientes()==0)
-				break;
-			Mensaje m=buffer.atender();
-			synchronized (m) {
-				m.setMensaje(m.getMensaje()+100);
-				m.notifyAll();
+			while(buffer.getCantidadClientes()>0)
+			{
+				Mensaje m = buffer.atender();
+				if(m != null)
+				{
+					m.setMensaje(m.getMensaje()+100);;
+				}
+				else
+				{
+					yield();
+				}
 			}
 		}
 	}
+	
+//	public void run()
+//	{
+//		while(true)
+//		{
+//			if(buffer.getCantidadClientes()==0)
+//				break;
+//			Mensaje m=buffer.atender();
+//			synchronized (m) {
+//				m.setMensaje(m.getMensaje()+100);
+//				m.notifyAll();
+//			}
+//		}
+//	}
 }
